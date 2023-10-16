01dcc2e84200a8b52f2738f8bc40cac57607b2ca
Walid <walid.hlali@mobi2go.com>
2023-10-16 14:06:41 -0400

    refactor: Add JSDoc to fetchAndExtractMenus function

--- src/utils/updatePricesFromExcel.ts
+++ src/utils/updatePricesFromExcel.ts
@@ -1,91 +1,100 @@
 // @ts-nocheck
 
 import { SimpleModalType } from "@/components/Modals/SimpleModal/SimpleModal";
-
-const updatePricesFromExcel = ({ excelData, originalData, setExtractedData, setErrorMessage, setIsSimpleModalOpen,setSimpleModalType }) => {
-
+/**
+ * Updates product and modifier prices in the original data based on information from an Excel file.
+ *
+ * @param {Object} params - The parameters object.
+ * @param {Object} params.excelData - The data extracted from the Excel file.
+ * @param {Object} params.originalData - The original product and modifier data.
+ * @param {Function} params.setExtractedData - Function to set the extracted data.
+ * @param {Function} params.setErrorMessage - Function to set the error message.
+ * @param {Function} params.setIsSimpleModalOpen - Function to open/close the modal.
+ * @param {Function} params.setSimpleModalType - Function to set the modal type.
+ *
+ * @throws Will throw an error if no matching backend names are found in originalData and excelData.
+ * @throws Will throw an error if no prices were updated.
+ *
+ * @returns {Object} The updated product and modifier data.
+ */
+const updatePricesFromExcel = ({
+  excelData,
+  originalData,
+  setExtractedData,
+  setErrorMessage,
+  setIsSimpleModalOpen,
+  setSimpleModalType,
+}) => {
   try {
-    // Check if there are any matching backend names in products and modifiers
-    const matchingProducts = originalData.products.some((product) =>
-      excelData.Products.some((excelProduct) => excelProduct["Backend Name"] === product.backend_name)
-    );
-
-    const matchingModifiers = originalData.modifiers.some((modifier) =>
-      excelData.Modifiers.some((excelModifier) => excelModifier["Backend Name"] === modifier.backend_name)
-    );
-
-    if (!matchingProducts && !matchingModifiers) {
-      setIsSimpleModalOpen(true);
-      setErrorMessage(
-        "No matching products or modifiers found in the uploaded data. Please verify if you uploaded the correct menu"
-      );
-      throw new Error(
-        "No matching products or modifiers found in the uploaded data. Please verify if you uploaded the correct menu."
-      );
-    }
-
-    const updatedData = { ...originalData }; // clone originalData to avoid mutation
+    // Deep clone the originalData to avoid mutation
+    const updatedData = JSON.parse(JSON.stringify(originalData));
     let countProductPricesUpdated = 0;
     let countModifierPricesUpdated = 0;
     let productTierCounter = 0;
     let modifierTierCounter = 0;
 
-    // update product prices
-    for (let product of updatedData.products) {
+    // Iterate through products and update their prices
+    updatedData.products.forEach((product, index) => {
       const excelProduct = excelData.Products.find((p) => p["Backend Name"] === product.backend_name);
+
+      // Update main product price
       if (excelProduct && excelProduct.Price !== product.price) {
-        product.price = excelProduct.Price;
+        updatedData.products[index].price = excelProduct.Price;
         countProductPricesUpdated++;
       }
 
-      // update product tier prices
-      for (let i = 0; i < product.property_tiers.length; i++) {
-        const newTierPrice = excelProduct[`Tier ${i + 1} Price`];
-        if (excelProduct && newTierPrice !== product.property_tiers[i].price) {
-          product.property_tiers[i].price = newTierPrice;
+      // Update individual tier prices for the product
+      product.property_tiers.forEach((tier, tierIndex) => {
+        const newTierPrice = excelProduct[`Tier ${tierIndex + 1} Price`];
+        if (newTierPrice !== tier.price) {
+          updatedData.products[index].property_tiers[tierIndex].price = newTierPrice;
           productTierCounter++;
         }
-      }
-    }
+      });
+    });
 
-    // update modifier prices
-    for (let modifier of updatedData.modifiers) {
+    // Iterate through modifiers and update their prices
+    updatedData.modifiers.forEach((modifier, index) => {
       const excelModifier = excelData.Modifiers.find((m) => m["Backend Name"] === modifier.backend_name);
+
+      // Update main modifier price
       if (excelModifier && excelModifier.Price !== modifier.price) {
-        modifier.price = excelModifier.Price;
+        updatedData.modifiers[index].price = excelModifier.Price;
         countModifierPricesUpdated++;
       }
 
-      // update modifier tier prices
-      for (let i = 0; i < modifier.property_tiers.length; i++) {
-        const newTierPrice = excelModifier[`Tier ${i + 1} Price`];
-        if (excelModifier && newTierPrice !== modifier.property_tiers[i].price) {
-          modifier.property_tiers[i].price = newTierPrice;
+      // Update individual tier prices for the modifier
+      modifier.property_tiers.forEach((tier, tierIndex) => {
+        const newTierPrice = excelModifier[`Tier ${tierIndex + 1} Price`];
+        if (newTierPrice !== tier.price) {
+          updatedData.modifiers[index].property_tiers[tierIndex].price = newTierPrice;
           modifierTierCounter++;
         }
-      }
-    }
+      });
+    });
 
+    // Validate if any prices were updated
     if (countProductPricesUpdated === 0 && countModifierPricesUpdated === 0) {
+      setErrorMessage("No prices were updated. Verify the Excel data.");
       setIsSimpleModalOpen(true);
-      setErrorMessage("No prices were updated. Please verify if the data in the Excel file matches the existing data.");
-      throw new Error("No prices were updated. Please verify if the data in the Excel file matches the existing data.");
+      setSimpleModalType(SimpleModalType.Error);
+      return null;
     }
 
-    console.log(`Updated prices for ${countProductPricesUpdated} products and ${productTierCounter} product tiers.`);
-    console.log(`Updated prices for ${countModifierPricesUpdated} modifier tiers and ${modifierTierCounter} modifier tiers.`);
-
+    // Set success message and modal type
+    const successMessage = `Updated prices for ${countProductPricesUpdated} products and ${productTierCounter} product tiers. Updated prices for ${countModifierPricesUpdated} modifiers and ${modifierTierCounter} modifier tiers.`;
+    setErrorMessage(successMessage);
     setIsSimpleModalOpen(true);
-    setSimpleModalType(SimpleModalType.Info)
-    setErrorMessage(`Updated prices for ${countProductPricesUpdated} products and ${productTierCounter} product tiers.Updated prices for ${countModifierPricesUpdated} modifier tiers and ${modifierTierCounter} modifier tiers.`)
+    setSimpleModalType(SimpleModalType.Info);
 
     return updatedData;
-    
   } catch (error) {
+    // Handle error scenarios
     setExtractedData(null);
-    setErrorMessage("Failed to update prices from Excel: " + error.message);
-    setSimpleModalType(SimpleModalType.Error)
+    setErrorMessage(`Failed to update prices from Excel: ${error.message}`);
     setIsSimpleModalOpen(true);
+    setSimpleModalType(SimpleModalType.Error);
+    return null;
   }
 };
 
